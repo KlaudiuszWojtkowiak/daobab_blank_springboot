7f01972bd5abb2bab86875b8cbf51031996b27c8
Klaudiusz <klaudiusz.wojtkowiak@whirly.pl>
2019-02-23 17:48:56 +0100

    fix

--- src/main/java/io/daobab/example/springboot/blank/SpringBootWebApplication.java
+++ src/main/java/io/daobab/example/springboot/blank/SpringBootWebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages={"io.daobab"})
 public class SpringBootWebApplication {
 
-    //Run it visit http://localhost:9111/test/t01
+    //Run it visit  http://localhost:9111/test/t01
 	public static void main(String[] args)  {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
