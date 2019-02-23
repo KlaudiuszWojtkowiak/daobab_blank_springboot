7e9fd0e6ccff5f22f46a0b24cb8d3c56a2bac4de
Klaudiusz <klaudiusz.wojtkowiak@whirly.pl>
2019-02-23 13:39:56 +0100

    initial commit

--- src.main/io/daobab/example/springboot/blank/dao/DaobabCreator.java
+++ src.main/io/daobab/example/springboot/blank/dao/DaobabCreator.java
@@ -1,50 +1,57 @@
 package io.daobab.example.springboot.blank.dao;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import io.daobab.generator.ClassGenerator;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 //@Configuration
 //@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class DaobabCreator {
 
-    private static String SCHEMA="";
-    private static String JAVA_PACKAGE="io.daobab.example.springboot.blank.dao";
-    private static String SOURCE_FOLDER="C:\\Users\\Klaudiusz\\IdeaProjects\\elephant\\daobab2-example\\src\\main\\java\\io\\daobab\\example\\dao";
+    private static String IP="";
+    private static String PORT="";
+    private static String USER="";
+    private static String PASS="";
+
+   private static String SCHEMA="";
+   private static String JAVA_PACKAGE="io.daobab.example.springboot.blank.dao";
+   private static String FILE_DIRECTIRY_PATH ="C:\\daobab";
+
 
 
     public static void main(String[] args){
-        ClassGenerator cg=new ClassGenerator(mySQLDatabase(),SCHEMA,JAVA_PACKAGE,SOURCE_FOLDER);
+        ClassGenerator cg=new ClassGenerator(mySQLDatabase());
+        cg.setSchema(SCHEMA);
+        cg.setJavaPackage(JAVA_PACKAGE);
+        cg.setFileDirectoryPath(FILE_DIRECTIRY_PATH);
+        cg.setGenerateTypeScriptClasses(true);
 
     }
 
     private static DataSource mySQLDatabase(){
 
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setDatabaseName("");
-        dataSource.setUser("root");
-        dataSource.setPassword("admin");
-        dataSource.setURL("jdbc:mysql://127.0.0.1:3306/pizza?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-
-        return dataSource;
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://"+IP+":"+PORT+"/"+SCHEMA+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        config.setUsername(USER);
+        config.setPassword(PASS);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.setSchema(SCHEMA);
+        return new HikariDataSource( config );
 
     }
 
 
     private static DataSource oracleDatabase(){
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl( "jdbc:oracle:thin://@10.0.0.8:1521/orcl" );
-        config.setUsername( "USERMANAG" );
-        config.setPassword( "YZgTL4mc" );
+        config.setJdbcUrl( "jdbc:oracle:thin://@IP:PORT/orcl" );
+        config.setUsername( USER );
+        config.setPassword( PASS );
         config.setDriverClassName("oracle.jdbc.OracleDriver");
         config.setSchema(SCHEMA);
-        config.addDataSourceProperty( "cachePrepStmts" , "true" );
-        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
-        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
         return new HikariDataSource( config );
     }
 }
